0a59dc597f9752456a5c001f7d2bdae6e8ab3837
Awet Fessha <awetkifle94@gmail.com>
2018-04-28 13:37:15 +0100

    finished writers-serch test

--- 08.writers-search/writers-search.js
+++ 08.writers-search/writers-search.js
@@ -1,1 +1,11 @@
 // check the tests and write code to make it pass
+function writersSearch(writers, name) {
+  newName = name.toLowerCase();
+  return writers.filter(function(writer) {
+    return (
+      writer.lastName.toLowerCase() === newName ||
+      writer.firstName.toLowerCase() === newName
+    );
+  });
+}
+module.exports = writersSearch;
