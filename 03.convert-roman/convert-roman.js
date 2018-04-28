dbf7ffab8e87ecec7d8a72f536d5ab1bcaca3fc1
Islam Dudaev <islamwolf23@gmail.com>
2018-04-28 12:10:15 +0100

    complete the 3 tests

--- 03.convert-roman/convert-roman.js
+++ 03.convert-roman/convert-roman.js
@@ -4,7 +4,19 @@
  * (only handle cases from 1 to 10)
  */
 function convert(number) {
-  // code
+  var numbers = [ 10, 9, 5, 4, 1 ];
+  var roman = [ 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var romanNumber = '';
+
+  for (var i = 0; i < numbers.length; i++) {
+    while (numbers[i] <= number) {
+      romanNumber += roman[i];
+      number -= numbers[i];
+    }
+  }
+
+  return romanNumber;
 }
 
 module.exports = convert;
