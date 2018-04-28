cae9270d8958de8fba89465d8152bb60d62b9213
matl2018 <lumierematerne11@gmail.com>
2018-04-28 13:20:44 +0100

    full-name passed

--- 02.full-names/full-name.js
+++ 02.full-names/full-name.js
@@ -3,11 +3,12 @@
  * It will return a full name with title
  * i.e. if passed "Mimi", "Popa", "Her Excellency"
  * then it will return "Her Excellency Mimi Popa"
- * 
+ *
  * Hint: Notice there is space after each argument, i.e. it is not HerExcellencyMimiPopa
  */
-function fullName(firstName, lastName, title) {
-  // code
-}
+const fullName = (firstName, lastName, title) => {
+  let fullName = title + " " + firstName + " " + lastName;
+  return fullName;
+};
 
 module.exports = fullName;
