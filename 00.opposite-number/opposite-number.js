ebee6e0af06e2ca06816022e70e10ac250b0df9a
tchapda daniella <dany2kul@yahoo.com>
2018-04-28 12:33:39 +0100

    dany's exe00

--- 00.opposite-number/opposite-number.js
+++ 00.opposite-number/opposite-number.js
@@ -3,9 +3,10 @@
  * i.e. if given 1, it will return -1
  * if given -1, it will return 1
  * 
- * Hints: Think what do  you need to do convert a number to its opposite (multiply by what?)
+ * Hints: Think what do  you need to dnpm testo convert a number to its opposite (multiply by what?)
  */
 function oppositeNumber(number) {
+  return (-number);
   // code here
 }
 
