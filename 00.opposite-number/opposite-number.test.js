8d3d9e056ccb63ea928763df5d32b842ecfa307f
matl2018 <lumierematerne11@gmail.com>
2018-04-28 12:28:44 +0100

    Opposite number exercise passed

--- 00.opposite-number/opposite-number.test.js
+++ 00.opposite-number/opposite-number.test.js
@@ -15,3 +15,10 @@ test("returns -40 if passed 40", function() {
 
 // add a test for  0
 // it should return zero if passed zero
+
+test("returns 0 if passed 0", function() {
+  var number = 0;
+  var opposite = oppositeNumber(0);
+
+  expect(opposite).toEqual(0);
+});
