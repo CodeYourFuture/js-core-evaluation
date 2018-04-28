96627d9126bdd49659aaac8241240d9acfe2a4cc
mnur86 <mnur_86@icloud.com>
2018-04-28 16:21:23 +0100

    ex5 & 6 done

--- 06.find-alive/find-alive.js
+++ 06.find-alive/find-alive.js
@@ -1,9 +1,14 @@
 /**
  * A function that takes an array of Writers objects and finds the ones who are alive
- * 
- * Think which array method we need to use (map, forEach, filter, find, every) 
+ *
+ * Think which array method we need to use (map, forEach, filter, find, every)
  *
  */
-var findAlive = function(writers) {};
+var findAlive = function(writers) {
+  var aliveOnes = writers.filter(function(person) {
+    if (person.alive === true) return true;
+  });
+  return aliveOnes;
+};
 
 module.exports = findAlive;
