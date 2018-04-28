a8383ce7e556c790f2f6f2dcfb83655c844bedc2
tchapda daniella <dany2kul@yahoo.com>
2018-04-28 13:45:13 +0100

    convert2roman

--- 03.convert-roman/convert-roman.js
+++ 03.convert-roman/convert-roman.js
@@ -4,7 +4,36 @@
  * (only handle cases from 1 to 10)
  */
 function convert(number) {
+  if(number === 1){
+    return "I";
+  }
+  else if (number === 2) {
+    return "II";
+  }
+  else if (number === 3) {
+    return "III";
+  }
+  else if (number === 4) {
+    return "IV";
+  }
+  else if (number === 5) {
+    return "V";
+  }
+ else if (number === 6) {
+    return "VI";
+  }
+  else if (number === 7) {
+    return "VII";
+  }
+  else if (number === 8) {
+    return "VIII";
+  }
+  else if (number === 9) {
+    return "IX";
+  }
+  else if (number === 10) {
+    return "X";
+  }
   // code
 }
-
 module.exports = convert;
