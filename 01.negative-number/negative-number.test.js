9665e2475f8b1bf21ae513057c9301b4b237bf46
matl2018 <lumierematerne11@gmail.com>
2018-04-28 13:03:20 +0100

    negative number passed

--- 01.negative-number/negative-number.test.js
+++ 01.negative-number/negative-number.test.js
@@ -5,8 +5,13 @@ test("returns -20 if passed -20", function() {
   expect(negative).toEqual(-20);
 });
 
-test("returns -40 if passed -40", function() {
+test("returns 40 if passed -40", function() {
   var negative = negativeNumber(40);
 
   expect(negative).toEqual(-40);
+});
+
+test("returns 0 if passed 0", function() {
+  var negative = negativeNumber(0);
+  expect(negative).toEqual(0);
 });
