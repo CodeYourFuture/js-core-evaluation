f6360a24af5dd59bbf97a8dc253ecb0473e652bb
Awet Fessha <awetkifle94@gmail.com>
2018-04-28 12:29:37 +0100

    finished full-name test

--- 02.full-names/full-name.js
+++ 02.full-names/full-name.js
@@ -3,11 +3,11 @@
  * It will return a full name with title
  * i.e. if passed "Mimi", "Popa", "Her Excellency"
  * then it will return "Her Excellency Mimi Popa"
- * 
+ *
  * Hint: Notice there is space after each argument, i.e. it is not HerExcellencyMimiPopa
  */
 function fullName(firstName, lastName, title) {
-  // code
+  return title + " " + firstName + " " + lastName;
 }
 
 module.exports = fullName;
