0a8d7c28e0f6f8bf4bcc2f635ec6e04e4fb1f68f
Islam Dudaev <islamwolf23@gmail.com>
2018-04-28 15:07:45 +0100

    change convert-roman file

--- 03.convert-roman/convert-roman.js
+++ 03.convert-roman/convert-roman.js
@@ -4,19 +4,8 @@
  * (only handle cases from 1 to 10)
  */
 function convert(number) {
-  var numbers = [ 10, 9, 5, 4, 1 ];
-  var roman = [ 'X', 'IX', 'V', 'IV', 'I' ];
-
-  var romanNumber = '';
-
-  for (var i = 0; i < numbers.length; i++) {
-    while (numbers[i] <= number) {
-      romanNumber += roman[i];
-      number -= numbers[i];
-    }
-  }
-
-  return romanNumber;
+  var roman = [ '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X' ];
+  return roman[number];
 }
 
 module.exports = convert;
