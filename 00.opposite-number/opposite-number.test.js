152d540b7380a838c40de448775e4f1501efa1a0
Soha <s.azadi12319@gmail.com>
2018-04-28 12:47:02 +0100

    opposite

--- 00.opposite-number/opposite-number.test.js
+++ 00.opposite-number/opposite-number.test.js
@@ -12,6 +12,11 @@ test("returns -40 if passed 40", function() {
 
   expect(opposite).toEqual(-40);
 });
+test("return 0 if passed 0", function() {
+  var number = 0;
+  var opposite = oppositeNumber(number);
 
+  expect(opposite).toEqual(-0);
+});
 // add a test for  0
 // it should return zero if passed zero
