6e4f43edac382f4d2b8a1f6c2f79a0556e78cd7c
Islam Dudaev <islamwolf23@gmail.com>
2018-04-28 12:44:44 +0100

    complete the 8 tests

--- 08.writers-search/writers-search.js
+++ 08.writers-search/writers-search.js
@@ -1,1 +1,14 @@
 // check the tests and write code to make it pass
+var findWriter = function(persons, writer) {
+    return persons.filter(person => {
+        if(person.firstName.toLowerCase() === writer.toLowerCase()){
+            return true;
+        }
+        if(person.lastName.toLowerCase() === writer.toLowerCase()){
+            return true;
+        }
+        return false;
+    });
+};
+
+module.exports = findWriter;
