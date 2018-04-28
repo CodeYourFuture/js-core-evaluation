5e810933a4ebc8c3af69ce0aecd57471cb9e2fa6
Awet Fessha <awetkifle94@gmail.com>
2018-04-28 13:00:45 +0100

    Finished remove-letter test

--- 05.remove-letter/remove-letter.js
+++ 05.remove-letter/remove-letter.js
@@ -1,12 +1,21 @@
 /**
  * The function takes two parameters: a sentence and a letter
  * it will return a new sentence with all instances of the letter removed
- * 
+ *
  * Hint: this is similar to removeVowels EXCEPT that we don't remove  vowels
  * instead, we remove whatever letter is passed to the function
  */
 function removeLetter(sentence, letter) {
-  // code
+  var characters = sentence.split("");
+  var result = [];
+  for (i = 0; i < characters.length; i++) {
+    if (characters[i] === letter) {
+      result.push("");
+    } else {
+      result.push(characters[i]);
+    }
+  }
+  return result.join("");
 }
 
 module.exports = removeLetter;
