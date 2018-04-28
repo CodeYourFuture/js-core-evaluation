08e78d5fd1c3e796c90f32d6c906695f2f4c7c31
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-04-28 13:03:11 +0100

    Palindrome

--- 04.palindrome/palindrome.js
+++ 04.palindrome/palindrome.js
@@ -5,7 +5,11 @@
  * Hint: Think how you would do this on paper, is there a JavaScript string method to help you do the same?
  */
 var isPalindrome = function(word) {
-  // code
+  wordPalindrome= word.split('').reverse().join('');// code
+  if (word === wordPalindrome)
+  {return true;}
+  else 
+  return false;
 };
 
 module.exports = isPalindrome;
