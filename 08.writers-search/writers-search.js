dac35c143dc80b15f99180780f3b6edcf9737bd2
Islam Dudaev <islamwolf23@gmail.com>
2018-04-28 21:56:38 +0100

    Refactor writers-search.js code

--- 08.writers-search/writers-search.js
+++ 08.writers-search/writers-search.js
@@ -1,14 +1,9 @@
 // check the tests and write code to make it pass
 var findWriter = function(persons, writer) {
-    return persons.filter(person => {
-        if(person.firstName.toLowerCase() === writer.toLowerCase()){
-            return true;
-        }
-        if(person.lastName.toLowerCase() === writer.toLowerCase()){
-            return true;
-        }
-        return false;
-    });
+    return persons.filter(person => 
+        person.firstName.toLowerCase() === writer.toLowerCase() || 
+        person.lastName.toLowerCase() === writer.toLowerCase()
+    );
 };
 
 module.exports = findWriter;
