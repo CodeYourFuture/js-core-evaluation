3554523ab875179d9e5bf1bbfe829b9d354b31f3
mnur86 <mnur_86@icloud.com>
2018-04-28 16:31:11 +0100

    ex7 is complete

--- 07.writers-names/writers-names.js
+++ 07.writers-names/writers-names.js
@@ -1,9 +1,14 @@
 /**
  * The function takes an array of objects (array of writers)
  * and returns an array (same length) with the full names of the writers
- * 
+ *
  * Hint: which array method returns the same length of items
  */
-var getWritersNames = function(writers) {};
+var getWritersNames = function(writers) {
+  return writers.map(
+    getWritersNames =>
+      getWritersNames.firstName + " " + getWritersNames.lastName
+  );
+};
 
 module.exports = getWritersNames;
