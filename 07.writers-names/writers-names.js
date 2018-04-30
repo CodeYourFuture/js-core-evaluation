973866b3742e620b0ccc20ed78fae9c3002b0021
matl2018 <lumierematerne11@gmail.com>
2018-04-30 22:58:31 +0100

    7. writers-names passed

--- 07.writers-names/writers-names.js
+++ 07.writers-names/writers-names.js
@@ -1,9 +1,14 @@
 /**
  * The function takes an array of objects (array of writers)
  * and returns an array (same length) with the full names of the writers
- * 
+ *
  * Hint: which array method returns the same length of items
  */
-var getWritersNames = function(writers) {};
+var getWritersNames = function(writers) {
+  return writers.map(function(writer) {
+    var fullName = writer.firstName + " " + writer.lastName;
+    return fullName;
+  });
+};
 
 module.exports = getWritersNames;
