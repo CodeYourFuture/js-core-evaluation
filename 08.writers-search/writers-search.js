7033d540d0cd9f55b9036f4efae8b19f02a1d829
mnur86 <mnur_86@icloud.com>
2018-04-30 18:58:08 +0100

    ex 9 is done

--- 08.writers-search/writers-search.js
+++ 08.writers-search/writers-search.js
@@ -1,1 +1,15 @@
 // check the tests and write code to make it pass
+var findWriter = function(persons, name) {
+    
+    // name = name[0].toUpperCase() + name.substring(1);
+
+    var result =persons.filter(person => {
+        if (person.firstName.toLowerCase() === name.toLowerCase() || person.lastName.toLowerCase() === name.toLowerCase()) {
+            return true;
+        }
+        return false;
+    })
+    return result;
+}
+
+module.exports = findWriter;
--- 09.dom/script.js
+++ 09.dom/script.js
@@ -16,3 +16,17 @@ var url = "https://api.giphy.com/v1/gifs/trending?api_key=" + apiKey;
     Hint: the image url can be found under `images.fixed_width_small.url`
     Another Hint: Always have  your Developer tools open, and check for errors and inspect the DOM and the JSON format.
 */
+fetch(url)
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(snap) {
+    var frameBox = document.querySelector("#snap");
+
+    snap.data.forEach(giphy => {
+      var snappyURL = giphy.images.fixed_width_small.url;
+      var image = document.createElement("img");
+      image.setAttribute("src", snappyURL);
+      frameBox.appendChild(image);
+    });
+  });
