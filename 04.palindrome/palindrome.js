bb9e4f27b14ba74c4f99858160c75e01f3159142
Won Maung Thein <wonmaungthein@gmail.com>
2018-05-01 17:15:01 +0100

    Solved palindrome exercise

--- 04.palindrome/palindrome.js
+++ 04.palindrome/palindrome.js
@@ -1,11 +1,24 @@
 /**
  * A function that takes a word and decides if it is a palindrome or not
  * A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward, such as madam or racecar.
- * 
+ *
  * Hint: Think how you would do this on paper, is there a JavaScript string method to help you do the same?
  */
 var isPalindrome = function(word) {
   // code
+  var reversedWord = word
+    .split("")
+    .reverse()
+    .join("");
+
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+  return reversedWord;
 };
 
 module.exports = isPalindrome;
+
+console.log(isPalindrome("woueuerd"));
