21ab8b9fa485d065377ad55ef11ee9ed5f5aff94
Won Maung Thein <wonmaungthein@gmail.com>
2018-05-01 18:49:14 +0100

    Solved remove letter exercise. The most joy I ever have after solving this challenge

--- 05.remove-letter/remove-letter.js
+++ 05.remove-letter/remove-letter.js
@@ -1,12 +1,23 @@
 /**
  * The function takes two parameters: a sentence and a letter
  * it will return a new sentence with all instances of the letter removed
- * 
+ *
  * Hint: this is similar to removeVowels EXCEPT that we don't remove  vowels
  * instead, we remove whatever letter is passed to the function
  */
 function removeLetter(sentence, letter) {
   // code
+  const splittedArray = sentence.split("");
+  const carArray = splittedArray.map(function(item) {
+    if (item === letter) {
+      return item + letter;
+    } else {
+      return item;
+    }
+  });
+
+  const result = carArray.filter(word => word.length < 2);
+  return result.join("");
 }
 
 module.exports = removeLetter;
