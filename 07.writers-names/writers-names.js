9b3397602e1924756cc90897bf614868fdb67a40
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-05-01 20:23:39 +0100

    writers-names

--- 07.writers-names/writers-names.js
+++ 07.writers-names/writers-names.js
@@ -1,9 +1,14 @@
 /**
  * The function takes an array of objects (array of writers)
  * and returns an array (same length) with the full names of the writers
- * 
+ *
  * Hint: which array method returns the same length of items
  */
 var getWritersNames = function(writers) {};
+var getWritersNames = function(writers) {
+  return writers.map(writer => {
+    return writer.firstName + " " + writer.lastName;
+  });
+};
 
 module.exports = getWritersNames;
