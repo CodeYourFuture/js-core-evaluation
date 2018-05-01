4eab80977b79311a21a57a302b1b5fa9493cd4b4
Won Maung Thein <wonmaungthein@gmail.com>
2018-05-01 19:29:09 +0100

    Solved writers name challenge

--- 07.writers-names/writers-names.js
+++ 07.writers-names/writers-names.js
@@ -1,9 +1,29 @@
 /**
  * The function takes an array of objects (array of writers)
  * and returns an array (same length) with the full names of the writers
- * 
+ *
  * Hint: which array method returns the same length of items
  */
-var getWritersNames = function(writers) {};
+
+var persons = [
+  {
+    firstName: "Gabriel Garcia",
+    lastName: "Marquez",
+    occupation: "writer",
+    age: 87,
+    alive: false
+  },
+  {
+    firstName: "Zadie",
+    lastName: "Smith",
+    occupation: "writer",
+    age: 42,
+    alive: true
+  }
+];
+
+var getWritersNames = function(writers) {
+  return writers.map(writer => `${writer.firstName} ${writer.lastName}`);
+};
 
 module.exports = getWritersNames;
