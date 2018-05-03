a9750becd66847d36f181a104d8ddb0e0197e395
Soha <s.azadi12319@gmail.com>
2018-05-03 17:29:14 +0100

    writers-names

--- 07.writers-names/writers-names.js
+++ 07.writers-names/writers-names.js
@@ -1,9 +1,14 @@
 /**
  * The function takes an array of objects (array of writers)
  * and returns an array (same length) with the full names of the writers
- * 
+ *
  * Hint: which array method returns the same length of items
  */
-var getWritersNames = function(writers) {};
+var getWritersNames = function(writers) {
+  var firstName = writers.firstName;
+  if (firstName.lenght() === lastName.lenght()) {
+    return push(firstName + "" + lastName);
+  } else return push("");
+};
 
 module.exports = getWritersNames;
