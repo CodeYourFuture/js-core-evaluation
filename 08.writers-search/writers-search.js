27417b6e07afbed280a1065a68005444bb81c206
matl2018 <lumierematerne11@gmail.com>
2018-05-04 18:24:25 +0100

    08 writers search passed

--- 08.writers-search/writers-search.js
+++ 08.writers-search/writers-search.js
@@ -1,1 +1,13 @@
 // check the tests and write code to make it pass
+var findWriter = function(persons, name) {
+  return persons.filter(function(person) {
+    var nameLowerCase = name.toLowerCase();
+    var firstNameLowerCase = person.firstName.toLowerCase();
+    var lastNameLoweCase = person.lastName.toLowerCase();
+    return (
+      firstNameLowerCase === nameLowerCase || lastNameLoweCase === nameLowerCase
+    );
+  });
+};
+
+module.exports = findWriter;
